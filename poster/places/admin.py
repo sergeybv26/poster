599e4bbfbfe6c2241cbdad1f9b9ba6841f6ffa93
sergeybv26 <sergey.bv26@gmail.com>
2022-10-26 13:50:03 +0300

    Add Place model

--- poster/places/admin.py
+++ poster/places/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import Place
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    exclude = ('uid',)
--- poster/places/models.py
+++ poster/places/models.py
@@ -1,3 +1,18 @@
+from uuid import uuid4
+
+from ckeditor.fields import RichTextField
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    """Модель сущности Достопримечательность"""
+    uid = models.UUIDField(primary_key=True, default=uuid4)
+    title = models.CharField(max_length=512, unique=True, verbose_name='Наименование')
+    description_short = models.CharField(max_length=512, blank=True, null=True, verbose_name='Краткое описание')
+    description_long = RichTextField(verbose_name='Полное описание')
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
