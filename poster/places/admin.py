2288052755d69533e17f9c02ab459a6ea5577422
sergeybv26 <sergey.bv26@gmail.com>
2022-10-27 12:33:20 +0300

    Add inline objects in admin Place

--- poster/places/admin.py
+++ poster/places/admin.py
@@ -3,10 +3,16 @@ from django.contrib import admin
 from places.models import Place, PlaceImage
 
 
+class ImageInlineAdmin(admin.TabularInline):
+    model = PlaceImage
+    exclude = ('uid', )
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
     exclude = ('uid',)
+    inlines = [ImageInlineAdmin, ]
 
 
 @admin.register(PlaceImage)
